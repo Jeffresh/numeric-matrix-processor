4d39ebaa1cf4924eb5507f8f95c0a07697b78718
Jeff <jeff.uca@gmail.com>
2020-07-29 16:21:38 +0200

    :sparkles: add stage 2/6 multiplication by number

---
+++ scalar_multiplication.py
@@ -0,0 +1,27 @@
+# Objectives
+#
+# Write a program that reads a matrix and a constant and outputs the result of their multiplication.
+#
+# The first line of the input contains the number of rows and the number of columns of the matrix.
+# Next lines contain rows of the matrix. The last line contains the constant.
+#
+# The constant and the elements of the matrix are integers.
+
+
+def scalar_multiplication(mat, scalar):
+    return [list(map(lambda x: x * scalar, mat[i])) for i in range(len(mat))]
+
+
+def print_matrix(mat):
+    for row in mat:
+        print(*row)
+
+
+if __name__ == '__main__':
+    a, b = list(map(int, input().split()))
+    mat_a = [list(map(int, input().split())) for _ in range(a)]
+    scalar = int(input())
+
+    mat_sc = scalar_multiplication(mat_a, scalar)
+
+    print(mat_sc)
