624e7da02e4054b13f901033aff5362a3064b95f
Jeff <jeff.uca@gmail.com>
2020-08-11 17:57:55 +0200

    :sparkles: add function to get de adj matrix

---
+++ inverse.py
@@ -0,0 +1,42 @@
+def get_minor(mat, row, column):
+    sub_mat = [row[:] for row in mat]
+    sub_mat.pop(row)
+    for row in range(len(sub_mat)):
+        sub_mat[row].pop(column)
+    return sub_mat
+
+
+def get_adj_mat(mat):
+    adj = []
+    if len(mat) == 1:
+        return mat[0][0]
+    if len(mat) == 2:
+        return mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0]
+    else:
+        for i in range(len(mat)):
+            adj.append([(-1) ** ((i + 1) + j + 1) * get_adj_mat(get_minor(mat, i, j)) for j in range(len(mat[0]))])
+
+    return adj
+
+
+def print_matrix(mat):
+    for row in mat:
+        print(*row)
+
+
+def get_dimensions(message):
+    return list(map(int, input(message).split()))
+
+
+def get_matrix(n_rows, message):
+    print(message)
+    return [list(map(float, input().split())) for _ in range(n_rows)]
+
+
+if __name__ == '__main__':
+    a, b = get_dimensions("Enter size of first matrix:")
+    mat_a = get_matrix(a, "Enter first matrix:")
+
+    adj_mat = get_adj_mat(mat_a)
+
+    print(adj_mat)
